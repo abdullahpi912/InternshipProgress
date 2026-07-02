364907704226a826c1ede40574c135d8b5e4b585
abdullahpi912 <abdullahpi912@gmail.com>
2026-07-02 10:54:47 +0530

    Day 1 : Task 1

---
+++ About_Me.py
@@ -0,0 +1,21 @@
+#Step 1: Declare your variables
+name = "Abdullah P I"
+age = 19
+Learn = "Developer Profile Using Python"
+Experience = "Beginner"
+Goal = "Artificial Intelligence Engineer"
+
+#Step 2: Check Data Types
+print ("Name Data Type: ",type(name))
+print ("Age Data Type :",type(age))
+
+#Step 3: Print the developer profile card
+print("========================================")
+print("DEVELOPER PROFILE CARD")
+print("========================================")
+
+print(f"Name                : {name}")
+print(f"Age                 : {age}")
+print(f"Learning Topic      : {Learn}")
+print(f"Coding Experience   : {Experience}")
+print(f"Goal                : {Goal}")
